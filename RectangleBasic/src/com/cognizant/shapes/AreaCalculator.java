d58a2d8a57ef17cfc02092f438b66c4612e2928b
MawnsterMawth <ashzheng2000@gmail.com>
2022-09-29 20:32:59 -0700

    First commit

---
+++ RectangleBasic/src/com/cognizant/shapes/AreaCalculator.java
@@ -0,0 +1,8 @@
+package com.cognizant.shapes;
+
+public class AreaCalculator {
+	public static void main(String arg[]){
+		Rectangle rectangle = new Rectangle();
+		rectangle.calculateArea();
+	}
+}
