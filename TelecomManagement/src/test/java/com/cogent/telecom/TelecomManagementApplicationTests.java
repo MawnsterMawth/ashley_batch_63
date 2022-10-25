aa57ac1d3186c0853d961b72d81bb7ab64b2058c
MawnsterMawth <ashzheng2000@gmail.com>
2022-10-25 08:44:01 -0700

    assessment

---
+++ TelecomManagement/src/test/java/com/cogent/telecom/TelecomManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogent.telecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
